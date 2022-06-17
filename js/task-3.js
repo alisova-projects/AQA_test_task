a385b0ea858d33ed4e3ba081b91d6ab06b0b6d71
Elena Alisova <63472862+alisova-projects@users.noreply.github.com>
2022-06-17 22:12:53 +0300

    task 4 done

--- js/task-3.js
+++ js/task-3.js
@@ -2,35 +2,12 @@
 Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3
 */
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function foo(arr) {
-  console.log(arr);
-  let newArr = [];
-  // let arrLength = arr.length;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 3 === 0) {
-      newArr.push(arr[i]);
-    }
-  }
-  console.log(newArr);
-}
-
-foo(arr);
-
-// ------------------------------------------------------------------
-//  пока не вышло реализовать заполнение массива данными с инпута, но я над этим ещё поработаю
-
-// const inputEl = document.querySelector("input#array-input");
-
-// inputEl.addEventListener("input", onInputChange);
-
-// function onInputChange(event) {
-//   const str = event.currentTarget.value;
-//   let arr = [];
-//   let arr1 = str.split(",").push(arr1)
+// function foo(arr) {
+//   console.log(arr);
 //   let newArr = [];
+//   // let arrLength = arr.length;
 
 //   for (let i = 0; i < arr.length; i++) {
 //     if (arr[i] % 3 === 0) {
@@ -39,3 +16,25 @@ foo(arr);
 //   }
 //   console.log(newArr);
 // }
+
+// foo(arr);
+
+// ------------------------------------------------------------------
+
+const inputEl = document.querySelector("input#array-input");
+
+inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  const str = event.currentTarget.value;
+  let newArr = [];
+  let arr = str.split(",");
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] % 3 === 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  console.log(newArr);
+  return;
+}
